6e9f7142d18dcf1f8d46ddc1b729a2fa4db0d57d
MUKILTEO\1717960 <1717960@mukilteo.wednet.edu>
2024-09-25 13:26:57 -0700

    HEALTH (hit band) UPGRADE

--- PM4-main/Assets/Gabby/Scripts/characterHealth.cs
+++ PM4-main/Assets/Gabby/Scripts/characterHealth.cs
@@ -6,8 +6,12 @@ using UnityEngine.SceneManagement;
 public class characterHealth : MonoBehaviour
 {
 
-    public float health = 3;
+    public float health = 5;
+    public GameObject healthFive;
+    public GameObject healthFour;
     public GameObject healthThree;
+    public GameObject healthThreeCope;
+    public GameObject healthThreeCope2;
     public GameObject healthTwo;
     public GameObject healthOne;
     //public GameObject loseScreen;
@@ -15,7 +19,11 @@ public class characterHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        healthFive.SetActive(true);
+        healthFour.SetActive(false);
         healthThree.SetActive(true);
+        healthThreeCope.SetActive(false);
+        healthThreeCope2.SetActive(false);
         healthTwo.SetActive(false);
         healthOne.SetActive(false);
         //loseScreen.SetActive(false);
@@ -27,19 +35,41 @@ public class characterHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (health == 2) //this assumes health was previously 3
+        if (health == 4) //this assumes health was previously 5
         {
+           // healthThreeCope2.SetActive(true);
+            healthFive.SetActive(false);
+            healthFour.SetActive(true);
+            healthThree.SetActive(true);
+           // healthTwo.SetActive(true);
+        }
+        if (health == 3) 
+        {
+            healthThreeCope2.SetActive(true);
+            healthThreeCope.SetActive(true);
+            healthFour.SetActive(false);
+            healthThree.SetActive(true);
+          //  healthTwo.SetActive(true);
+        }
+        if (health == 2) 
+        {
+            healthThreeCope2.SetActive(true);
+            healthThreeCope.SetActive(true);
             healthThree.SetActive(false);
             healthTwo.SetActive(true);
         }
         else if (health == 1)
         {
+            healthThreeCope2.SetActive(true);
+            healthThreeCope.SetActive(true);
             healthThree.SetActive(false);
             healthTwo.SetActive(false);
             healthOne.SetActive(true);
         }
         else if (health <= 0)
         {
+            healthThreeCope2.SetActive(false);
+            healthThreeCope.SetActive(false);
             healthThree.SetActive(false);
             healthTwo.SetActive(false);
             healthOne.SetActive(false);
